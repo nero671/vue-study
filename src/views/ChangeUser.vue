ac8d888ca010381f89df4df7c73a988b93600197
Nero <s.kachalko@imedia.by>
2022-05-16 16:49:02 +0300

    first commit / add changeUsers

---
+++ src/views/ChangeUser.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="changeUser">
+    <img :src="picture" :class="gender" :alt="`${firstName} ${lastName}`" />
+    <h1>{{firstName}} {{lastName}}</h1>
+    <h3>Email: {{email}}</h3>
+    <button @click="getUser()" :class="gender">Get Random User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'changeUser',
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'jogn@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/66.jpg'
+    }
+  },
+  methods: {
+    async getUser() {
+      const response = await fetch('https://randomuser.me/api/');
+      const { results } = await response.json();
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+  .changeUser {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 35px;
+  }
+
+  img {
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  button {
+    margin-top: 10px;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+  }
+
+  .male {
+    background: steelblue;
+    border: 5px solid steelblue;
+  }
+
+  .female {
+    background: pink;
+    border: 5px solid pink;
+    color: #333;
+  }
+
+</style>
---
+++ src/views/FormComponents.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="form">
+    <ul id="example-1">
+      <li v-for="item in data.items" :key="item.message" @click="click(item.message)">
+        {{ item.message }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FormComponents',
+    data() {
+      return {
+        data: {
+          items: [
+            {message: 'Foo'},
+            {message: 'Bar'}
+          ]
+        }
+      }
+    },
+    methods: {
+      click(message) {
+        console.log(message)
+      }
+    },
+  }
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
